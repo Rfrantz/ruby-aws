485dbdd9d753f4ea4105171e4322084fa3e4ecf1
Rfrantz <rgallotti@me.com>
2017-04-13 11:53:32 -0300

    Add files via upload

--- amazon.rb
+++ amazon.rb
@@ -3,9 +3,55 @@ require "aws-sdk-core"
 require "rubygems"
 require "io/console"
 require "pry"
-load "languages/pt-br.rb"
 load "classes/classes.rb"
 load "classes/classe-objeto.rb"
+listando = "
+                                          - LISTANDO BUCKETS...
+"
+opcoes = "
+                              ----------------------------------------------
+                              -------    Utilize os comando abaixo   -------
+                              ----------------------------------------------
+                              -------   delete  =>  DELETAR BUCKET   -------
+                              -------   listar  =>  LISTA BUCKETS    -------
+                              -------   criar   =>  CRIA BUCKET      -------
+                              -------   upload  =>  SOBE OBJETOS     -------
+                              -------   url     =>  BUCKET URL       -------
+                              -------   acesso  =>  ACESSOS          -------
+                              -------   baixar  =>  DOWNLOAD         -------
+                              -------   sair    =>  FECHA APP        -------
+"
+
+apresentacao = <<DOC
+
+
+
+
+                             - BEM VINDO A INTEGRAÇÃO AWS COM RUBY -
+
+                ***********************************************************************
+                *                                                                     *
+                *  ======= Este é um teste de interação com a AWS com o Ruby  ======  * 
+                *  ================   que permite os seguintes:   ==================  *
+                *                                                                     *
+                *  _________________________________________________________________  *
+                *                                                                     *
+                *                     | CRIAR BUCKET                                  *
+                *                     | URL DO BUCKET                                 *
+                *                     | DELETAR BUCKET                                *
+                *                     | UPLOAD DE ARQUIVO                             *
+                *                     | DOWNLOAD DE OBJETO                            *
+                *                     | LISTAR BUCKETS/OBJETOS                        *
+                *                                                                     *
+                ***********************************************************************
+                | Criado em Ruby [por] Raphael Gallotti Frantz
+
+
+DOC
+
+termino_periodo = "
+                  ==================================================================="
+
 
 operacao = "listar"
 region = "us-west-2"
---
+++ languages/pt-br.Appraisals
@@ -0,0 +1,46 @@
+listando = "
+                                          - LISTANDO BUCKETS...
+"
+opcoes = "
+                              ----------------------------------------------
+                              -------    Utilize os comando abaixo   -------
+                              ----------------------------------------------
+                              -------   delete  =>  DELETAR BUCKET   -------
+                              -------   listar  =>  LISTA BUCKETS    -------
+                              -------   criar   =>  CRIA BUCKET      -------
+                              -------   upload  =>  SOBE OBJETOS     -------
+                              -------   url     =>  BUCKET URL       -------
+                              -------   acesso  =>  ACESSOS          -------
+                              -------   baixar  =>  DOWNLOAD         -------
+                              -------   sair    =>  FECHA APP        -------
+"
+
+apresentacao = <<DOC
+
+
+
+
+                             - BEM VINDO A INTEGRAÇÃO AWS COM RUBY -
+
+                ***********************************************************************
+                *                                                                     *
+                *  ======= Este é um teste de interação com a AWS com o Ruby  ======  * 
+                *  ================   que permite os seguintes:   ==================  *
+                *                                                                     *
+                *  _________________________________________________________________  *
+                *                                                                     *
+                *                     | CRIAR BUCKET                                  *
+                *                     | URL DO BUCKET                                 *
+                *                     | DELETAR BUCKET                                *
+                *                     | UPLOAD DE ARQUIVO                             *
+                *                     | DOWNLOAD DE OBJETO                            *
+                *                     | LISTAR BUCKETS/OBJETOS                        *
+                *                                                                     *
+                ***********************************************************************
+                | Criado em Ruby [por] Raphael Gallotti Frantz
+
+
+DOC
+
+termino_periodo = "
+                  ==================================================================="
